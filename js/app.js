f9c2037b63432e21294b12449efe7de54366aab2
Zk433 <zk0433@gmail.com>
2017-03-08 09:44:10 +0000

    finished website version 1.0

---
+++ js/app.js
@@ -0,0 +1,22 @@
+function getHeight() {
+	var height = $('section.promo, section.promo-video').height();
+	$('#overlay').css({'height':height+'px'})
+}
+
+$(function() {
+
+	window.setTimeout('getHeight()',100);
+
+	$('#gallery a').on('click', function() {
+		var image = $(this).find('img').attr('src');
+		var imageSelected = image.split('thumbnail');
+		var fullPath = imageSelected[0] + 'fullsize' + imageSelected[1];
+		$('#galleryModal #imageContainer').attr('src', fullPath);
+	});
+});
+
+$(window).resize(function() {
+	getHeight();
+});
+
+$(document).foundation();
